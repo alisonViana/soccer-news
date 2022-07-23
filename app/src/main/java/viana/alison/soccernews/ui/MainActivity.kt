b6e00fe369ede968fb8802f04acabc980cc2593e
alisonViana <alisonviana.c@gmail.com>
2022-07-23 09:35:54 -0300

    Refactor main activity and create App application

--- app/src/main/java/viana/alison/soccernews/ui/MainActivity.kt
+++ app/src/main/java/viana/alison/soccernews/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package viana.alison.soccernews
+package viana.alison.soccernews.ui
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,21 +7,21 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import viana.alison.soccernews.R
 import viana.alison.soccernews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
